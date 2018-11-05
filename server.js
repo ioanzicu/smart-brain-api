778d3d700d1ff736fa2ae72d2080e459c7c64e45
Ioan <ioan.zicu94@gmail.com>
2018-11-05 06:48:44 +0100

    Changing response

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Root
-app.get('/', (req, res) => { res.send(database.users); });
+app.get('/', (req, res) => { res.send('it is working'); });
 
 // Sign In
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
