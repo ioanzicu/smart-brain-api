5b327ff431e6b4806e2aa3204efd70632a848455
Ioan <ioan.zicu94@gmail.com>
2018-11-05 08:21:59 +0100

    Try to fix image box

--- controllers/image.js
+++ controllers/image.js
@@ -3,9 +3,11 @@ const Clarifai = require('clarifai');
 const app = new Clarifai.App({apiKey: '3cda67ae0adb4ae6b169d3b5a3a7bb7a'});
 
 const handleApiCall = (req, res) => {
-	// if(!req.body.input) {
-	// 	return res.status(400);
-	// }
+	console.log('req', req);
+	console.log('res', res);
+	if(!req.body.input) {
+		return res.status(400);
+	}
 	app.models
 		.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
 		.then(data => {
